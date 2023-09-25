4282e540cfaa441fda06e00e4173ab4fb1c4d9f2
Vadim Niziev <st1mpro23@yandex.ru>
2023-09-26 01:19:48 +0300

    быстрый курс по вебпаку

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import webpack from './images/webpack.png'
+import classes from './styles/style.module.scss'
+
+export const App = () => {
+    return (
+        <>
+            <div className={classes.head}><h1>Webpack Course</h1></div>
+            <div className={classes.body}><img src={webpack} alt=""/></div>
+            <div className={classes.foot}><h2>Thanks</h2></div>
+        </>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+// const config = {} // тоже самое что и module.exports
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+require("@babel/polyfill");
+
+module.exports = {
+    mode:'development',
+    entry: ["@babel/polyfill",'./src/index.js'],
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname,'dist'),
+        clean: true
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname,'src/index.html')
+    })],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        open: true,
+        compress: true,
+        port: 9000,
+    },
+    resolve: {
+        extensions: ['.jsx','.js'],
+        preferAbsolute: true,
+        modules: [path.resolve(__dirname,'src'), 'node_modules'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.less$/i,
+                use: ["style-loader", "css-loader", "less-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader","sass-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            }
+        ],
+    },
+}
